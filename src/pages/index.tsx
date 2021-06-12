a0bf239d90d94f9bbffb7e0b47124efced8ac049
horri1520 <kh1520.shsf@gmail.com>
2021-06-13 03:02:23 +0900

    chore: add test index link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import FullWidthRoundedLink from '../components/atoms/links/full-width-rounded-link';
+import { pagePaths } from '../config/page-paths';
 
 const IndexPage: React.VFC = () => {
-  return <div>IndexPage</div>;
+  return (
+    <div>
+      IndexPage
+      <FullWidthRoundedLink href={pagePaths.test.index}>
+        テストTOPへ
+      </FullWidthRoundedLink>
+    </div>
+  );
 };
 
 export default IndexPage;
